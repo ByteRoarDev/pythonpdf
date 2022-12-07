6266036f6648fd420e7567ba4f4c4a1bb613918d
Pradeep Bande <pradeep@agomic.com>
2022-12-07 20:51:24 +0530

    second commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+   <body>
+      
+      <form action="" method="POST" enctype="multipart/form-data">
+        Select File <input type="file" name="testfile" />
+         <input type="submit" value="Uploaded File" />
+      </form>
+      
+   </body>
+</html>
---
+++ readFile.py
@@ -0,0 +1,78 @@
+import PyPDF2
+import re
+import mysql.connector
+
+
+pdfFileObj = open('./data/csnt18-2022.pdf', 'rb')
+  
+# creating a pdf reader object
+pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+  
+# printing number of pages in pdf file
+print(pdfReader.numPages)
+numPages = pdfReader.numPages
+# creating a page object
+# pageObj = pdfReader.getPage(0)
+# pageObj2 = pdfReader.getPage(1)
+
+# data = pageObj.extractText()
+# data2 = pageObj2.extractText()
+# splittedData = data.split() 
+# splittedData2 = data2.split() 
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="",
+  database="test"
+)
+mycursor = mydb.cursor()
+
+sql = "INSERT INTO currency_data (currency, import_price, export_price) VALUES (%s, %s, %s)"
+combined = [] #splittedData #+ splittedData2
+if numPages > 0 : 
+   j=0
+   while j < numPages:
+      pageObj = pdfReader.getPage(j)
+      data = pageObj.extractText()
+      splittedData = data.split()
+      combined += splittedData
+      j +=1
+
+   regexDigit = re.compile(r'^\d*\.')
+   i=0
+   while i < len(combined):
+      data = combined[i]
+      increment=5
+      import_price=''
+      export_price=''
+      if(regexDigit.match(combined[i]) and data[len(data)-1]=='.'):
+         printData = combined[i+1] + ' '
+         currency = combined[i+1] + ' '
+         if regexDigit.match(combined[i+2]):
+            printData += ' Import : ' + combined[i+2] + ' Export : ' + combined[i+3] 
+            import_price = combined[i+2]
+            export_price = combined[i+3] if float(combined[i+3]) > 0 else combined[i+4]
+         elif not(regexDigit.match(combined[i+2])) and regexDigit.match(combined[i+3]):
+            printData += combined[i+2] + ' Import : ' + combined[i+3] + ' Export : ' + combined[i+4]
+            currency += combined[i+2]
+            import_price = combined[i+3]
+            export_price = combined[i+4] if float(combined[i+4]) > 0 else combined[i+5]
+         elif regexDigit.match(combined[i+4]):
+            currency += combined[i+2] + ' ' + combined[i+3]
+            import_price = combined[i+4]
+            export_price = combined[i+5] #if float(combined[i+5]) > 0 else combined[i+6]
+            printData = currency + ' Import : ' + combined[i+4] + ' Export : ' + combined[i+5]
+            increment = 6
+         print(printData)
+         if len(import_price) > 0 and len(export_price) > 0 :
+            val = (currency, import_price, export_price)
+            mycursor.execute(sql, val)
+            mydb.commit()
+         i+=increment
+      else:
+         i+=1
+      
+
+# closing the pdf file object
+pdfFileObj.close()
